1d945018ba40d5bf4520ffdac97f0f8293b88c48
Gabriel Morales <gabrielmorales1@pm.me>
2020-08-26 00:56:03 -0500

    feat: Nuevo diseño de polybar, ahora en tema blanco

--- .config/polybar/scripts/cmus.sh
+++ .config/polybar/scripts/cmus.sh
@@ -3,4 +3,4 @@
 artist=$(echo -n $(cmus-remote -C status | grep artist -m 1| cut -c 12-))
 song=$(echo -n $(cmus-remote -C status | grep title | cut -c 11-))
 
-echo -n "$artist - $song"
+echo -n "$song"
---
+++ .config/polybar/scripts/network.sh
@@ -0,0 +1,7 @@
+wget -q --spider http://google.com
+
+if [ $? -eq 0 ]; then
+	echo ""
+else
+	echo ""
+fi
