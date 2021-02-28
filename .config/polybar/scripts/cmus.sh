2de89d8cd60d15591fa72d8737d192d3688973b5
Gabriel Morales <gabrielmorales1@pm.me>
2021-02-28 13:50:50 -0500

    Polybar: rediseño con nuevos colores

--- .config/polybar/scripts/cmus.sh
+++ .config/polybar/scripts/cmus.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-# artist=$(echo -n $(cmus-remote -C status | grep artist -m 1| cut -c 12-))
-song=$(echo -n $(cmus-remote -C status | grep title | cut -c 11-))
-
+song=$(cmus-remote -C status | grep title | cut -c 11-)
 echo -n "$song"
