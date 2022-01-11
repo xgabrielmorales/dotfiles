fdf5a08e5f803336eacfe0330e7b1b62215ba540
Gabriel Morales <gabrielmorales1@pm.me>
2022-01-10 19:00:03 -0500

    Envía una notificación con el ping hacia 8.8.8.8

--- scripts/network-status.sh
+++ scripts/network-status.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-PING=$(ping 8.8.8.8 -c 1 | tail -1 | cut -d "/" -f5 | cut -d "." -f1)
+PING=$(ping 8.8.8.8 -c 1 | sed -n "2p" | cut -d "=" -f 4)
 
-[[ "$PING" == "" ]] && echo " " || echo " $PING""ms"
+[[ "$PING" == "" ]] && \
+	notify-send "Network Status" " No Internet Connection" --expire-time 1200 || \
+	notify-send "Network Status" " $PING" --expire-time 1200
