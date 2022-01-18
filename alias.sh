3c0d21b9d9bf4be886dcf1340e49755b144b7b94
Gabriel Morales <gabrielmorales1@pm.me>
2022-01-18 12:54:54 -0500

    Mantenimiento a mis Aliases

--- alias.sh
+++ alias.sh
@@ -1,37 +1,29 @@
-#export TERM=xterm-256color
+#====================================================
+# VARIABLES DE ENTORNO
+#====================================================
 export EDITOR=nvim
 export VISUAL=nvim
 export READER=zathura
 export HISTORY_IGNORE="(ls|cd|pwd|exit|sudo reboot|history|cd -|cd ..)"
-
-#+====================================================+
+#====================================================
 # MODIFICACIONES
-#+====================================================+
+#====================================================
+# Reasignaciones
 alias cp="cp -i"
-alias rm="rm -i"
+alias rm="rm -I"
 alias vim="nvim"
-
-alias la="ls -og"
-alias ld="ls -lAF | grep '^d'"
 alias ls="ls -FXAhc --group-directories-first --time-style=+'%H:%M %d/%m/%y' --color=auto"
-
-alias man="man --locale=es"
-alias cat="bat"
+# Clipboard
 alias cbcopy="xclip -selection clipboard"
 alias cbpaste="xclip -selection clipboard -o"
-alias cnext="calcurse --next"
-
+# Utilidades
+alias pyenv="source .env/bin/activate"
+# Otros
 alias radio="curl https://coderadio-relay-blr.freecodecamp.org/radio/8010/radio.mp3 | mpg123 - 2> /dev/null"
-#+====================================================+
+#====================================================
 #  ATAJOS DE NAVEGACIÓN
-#+====================================================+
+#====================================================
 alias dl="cd ~/Downloads && ls -l"
 alias doc="cd ~/Documents && ls -l"
-alias wallpapers="cd ~/Pictures/wallpapers/ && ls -l"
-alias dotfiles="cd ~/dotfiles && ls -l"
-alias dconfig="cd ~/dotfiles/.config && ls -l"
-alias vimconfig="cd ~/.config/nvim/ && ls -l"
-alias chrome="cd ~/.mozilla/firefox/rszqgpto.default-release/chrome && ls -l"
 alias dev="cd ~/Documents/dev && ls -l"
-alias math="cd ~/Documents/math && ls -l"
-alias pyenv="source .env/bin/activate"
+alias dotfiles="cd ~/dotfiles && ls -l"
