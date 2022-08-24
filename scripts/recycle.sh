8ec281be6b8e7620963e5f85d0b22d3ffe9ae382
Gabriel Morales <gabrielmorales1@pm.me>
2022-08-23 21:18:06 -0500

    Scripts update

---
+++ scripts/recycle.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Instead of running a new program recycle the existing one.
+
+BINARY=$(which $1)
+WINDOW_NAME=$2
+
+WINDOW=$(wmctrl -l | grep "$WINDOW_NAME$" | cut -d " " -f 1)
+
+if [ -n "$WINDOW" ]; then
+	wmctrl -ir "$WINDOW" -b remove,hidden; wmctrl -ia "$WINDOW"
+else
+	$BINARY &
+fi
