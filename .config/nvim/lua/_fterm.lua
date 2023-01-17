5dbe7090a19bbc4725f707a6b079354f48633f43
Gabriel Morales <gabrielmorales1@pm.me>
2023-01-16 22:39:52 -0500

    Adjusting FTerm

--- .config/nvim/lua/_fterm.lua
+++ .config/nvim/lua/_fterm.lua
@@ -1,24 +1,31 @@
-require('FTerm').setup({
-    border = 'double',
-    dimensions  = {
-        height = 0.9,
-        width = 0.9,
-    },
+local fterm = require("FTerm")
+
+fterm.setup({
+  border = 'single',
+  dimensions  = {
+    width = 0.9,
+    height = 0.9,
+  },
 })
 
--- Example keybindings
-vim.keymap.set('n', '<leader>t', '<CMD>lua require("FTerm").toggle()<CR>')
-vim.keymap.set('t', '<leader>t', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
 
+-- Execute commands in a floating terminal
+local lazygit = fterm:new({ cmd = 'lazygit' })
 
--- Code Runner - execute commands in a floating terminal
-local runners = { python = {'python', '-q'} }
+local runners = {
+  python = { 'python', '-q' }
+}
 
 vim.keymap.set('n', '<leader><Enter>', function()
-    local buf = vim.api.nvim_buf_get_name(0)
-    local ftype = vim.filetype.match({ filename = buf })
-    local exec = runners[ftype]
-    if exec ~= nil then
-        require('FTerm').scratch({ cmd = exec })
-    end
+  local buf = vim.api.nvim_buf_get_name(0)
+  local ftype = vim.filetype.match({ filename = buf })
+  local exec = runners[ftype]
+  if exec ~= nil then
+    fterm.scratch({ cmd = exec })
+  end
 end)
+
+
+-- keybindings
+vim.keymap.set('n', '<leader>t', function() fterm.toggle() end)
+vim.keymap.set('n', '<leader>G', function() lazygit:toggle() end)
