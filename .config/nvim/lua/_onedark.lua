63c7af1983787e32872df481c2f6a2302cee6433
Gabriel Morales <gabrielmorales1@pm.me>
2023-01-16 22:40:30 -0500

    Adjusting onedark theme

--- .config/nvim/lua/_onedark.lua
+++ .config/nvim/lua/_onedark.lua
@@ -1,6 +1,10 @@
 require('onedark').setup({
   style = 'dark',
   code_style = {
-      strings = 'italic',
-  }
+    comments = 'italic',
+    keywords = 'none',
+    functions = 'none',
+    strings = 'none',
+    variables = 'none'
+  },
 })
