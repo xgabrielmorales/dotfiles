9a2dc9416e7b68ca2f25c8e8e4bb0d9d140fa253
Gabriel Morales <gabrielmorales1@pm.me>
2023-02-05 23:00:56 -0500

    Neovim: sysmbols outline config update

--- .config/nvim/lua/_symbols-outline.lua
+++ .config/nvim/lua/_symbols-outline.lua
@@ -1,5 +1,7 @@
 require("symbols-outline").setup({
-  width = 20,
+  width = 25,
+  show_symbol_details = false,
+  autofold_depth = 1,
 })
 
 vim.keymap.set('n', '<leader>T', ':SymbolsOutline<CR>', { silent = true })
