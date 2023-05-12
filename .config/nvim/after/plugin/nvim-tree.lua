484de44d441cbe82da1f7b4e85a6ec7df89dbe23
Gabriel Morales <gabrielmorales1@pm.me>
2023-05-12 16:51:48 -0500

    Updating nvim-tree config to new version

--- .config/nvim/after/plugin/nvim-tree.lua
+++ .config/nvim/after/plugin/nvim-tree.lua
@@ -3,37 +3,37 @@ if not ok then
   return
 end
 
+
+local function on_attach(bufnr)
+  local api = require('nvim-tree.api')
+
+  local function opts(desc)
+    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  vim.keymap.set('n', 'q', api.tree.close, opts('Close'))
+  vim.keymap.set('n', 'c', api.tree.change_root_to_node, opts('CD'))
+  vim.keymap.set('n', 'u', api.tree.change_root_to_parent, opts('Up'))
+  vim.keymap.set('n', '<CR>', api.node.open.no_window_picker, opts('Open: No Window Picker'))
+  vim.keymap.set('n', '<BS>', api.node.navigate.parent_close, opts('Close Directory'))
+  vim.keymap.set('n', 'W', api.tree.collapse_all, opts('Collapse'))
+  vim.keymap.set('n', 'ss', api.node.open.horizontal, opts('Open: Horizontal Split'))
+  vim.keymap.set('n', 'sv', api.node.open.vertical, opts('Open: Vertical Split'))
+  vim.keymap.set('n', 'r', api.fs.rename, opts('Rename'))
+  vim.keymap.set('n', '+', api.fs.create, opts('Create'))
+  vim.keymap.set('n', 'd', api.fs.remove, opts('Delete'))
+  vim.keymap.set('n', 'R', api.tree.reload, opts('Refresh'))
+  vim.keymap.set('n', '<C-h>', api.tree.toggle_hidden_filter, opts('Toggle Dotfiles'))
+
+end
+
 nvim_tree.setup({
+  on_attach = on_attach,
   sort_by = "case_sensitive",
   view = {
     width = 25,
     side = "right",
     adaptive_size = true,
-    hide_root_folder = true,
-
-    mappings = {
-      custom_only = true,
-      list = {
-        { key = "q", action = "close" },
-
-        { key = "c", action = "cd" },
-        { key = "u", action = "dir_up"},
-
-        { key = "<CR>", action = "edit_no_picker" },
-        { key = "<BS>", action = "close_node" },
-        { key = "W",    action = "collapse_all" },
-
-        { key = "ss", action = "split" },
-        { key = "sv", action = "vsplit" },
-
-        { key = "r", action = "rename" },
-        { key = "+", action = "create" },
-        { key = "d", action = "remove" },
-
-        { key = "R", action = "refresh" },
-        { key = "<C-h>", action = "toggle_dotfiles"},
-      },
-    },
   },
   actions = {
     open_file = {
@@ -41,6 +41,7 @@ nvim_tree.setup({
     }
   },
   renderer = {
+    root_folder_label = false,
     group_empty = true,
     indent_markers = {
       enable = true,
