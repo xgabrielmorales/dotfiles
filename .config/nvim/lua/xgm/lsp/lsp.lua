2370178ac58a1304d92f379e93d0fa96bd6f9e3a
Gabriel Morales <gabrielmorales1@pm.me>
2023-06-05 12:02:33 -0500

    LSP configuration

--- .config/nvim/lua/xgm/lsp/lsp.lua
+++ .config/nvim/lua/xgm/lsp/lsp.lua
@@ -1,6 +1,9 @@
 local lspconfig = require("lspconfig")
 local on_attach = require("xgm.lsp.attach")
 
+--------------------------------
+-- Python LSP Config
+--------------------------------
 -- Jedi Language Server
 -- https://github.com/pappasam/jedi-language-server
 lspconfig.jedi_language_server.setup({
@@ -10,13 +13,33 @@ lspconfig.jedi_language_server.setup({
       enable = false,
       didOpen = true,
       didChange = false,
-      didSave = true
+      didSave = true,
     },
   },
 })
 
--- Ruff Language Server
--- https://github.com/charliermarsh/ruff-lsp
-local ruff_lsp_settings = ({
+--------------------------------
+-- Rust LSP Config
+--------------------------------
+-- Rust Analyzer
+-- https://github.com/rust-lang/rust-analyzer
+lspconfig.rust_analyzer.setup({
   on_attach = on_attach,
+})
+
+--------------------------------
+-- Lua LSP Config
+--------------------------------
+--  Lua Language Server
+-- https://github.com/LuaLS/lua-language-server
+require("lspconfig").lua_ls.setup({
+  on_attach = on_attach,
+  settings = {
+    Lua = {
+      telemetry = { enable = false },
+      diagnostics = {
+        globals = { "vim", "require" },
+      },
+    },
+  },
 })
