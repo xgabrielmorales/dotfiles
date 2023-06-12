2396098a6dbd68a8efd12cba32e54c4fdfbaa09f
Gabriel Morales <gabrielmorales1@pm.me>
2023-06-12 13:04:38 -0500

    Barbecue

---
+++ .config/nvim/lua/plugins/barbecue.lua
@@ -0,0 +1,12 @@
+return {
+  "utilyre/barbecue.nvim",
+  dependencies = {
+    "SmiteshP/nvim-navic",
+    "nvim-tree/nvim-web-devicons",
+  },
+  opts = {
+    show_dirname = false,
+    show_basename = false,
+  },
+  ft = { "python" },
+}
