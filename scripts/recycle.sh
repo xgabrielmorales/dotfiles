a1463190dcc78f7805cf0d0cbee24c76d15f164d
Gabriel Morales <dev@darklock.maskmy.id>
2024-02-16 22:11:17 -0500

    [scripts] - Changing tabs to spaces.

--- scripts/recycle.sh
+++ scripts/recycle.sh
@@ -8,7 +8,7 @@ WINDOW_NAME=$2
 WINDOW=$(wmctrl -l | grep "$WINDOW_NAME$" | cut -d " " -f 1)
 
 if [ -n "$WINDOW" ]; then
-	wmctrl -ir "$WINDOW" -b remove,hidden; wmctrl -ia "$WINDOW"
+    wmctrl -ir "$WINDOW" -b remove,hidden; wmctrl -ia "$WINDOW"
 else
-	$BINARY &
+    $BINARY &
 fi
