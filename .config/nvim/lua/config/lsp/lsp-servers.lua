ba6d13c803c2aa47bd39d461d25db6e771b11e59
Gabriel Morales <dev@darklock.maskmy.id>
2024-03-29 13:25:23 -0500

    [nvim] - lsp configuration

--- .config/nvim/lua/config/lsp/lsp-servers.lua
+++ .config/nvim/lua/config/lsp/lsp-servers.lua
@@ -1,9 +1,22 @@
 local lspconfig = require("lspconfig")
 local M = require("config.lsp.lsp-config")
 
---------------------------------
--- Python LSP Config
---------------------------------
+local servers = {
+  "bashls",        -- https://github.com/bash-lsp/bash-language-server
+  "dockerls",      -- https://github.com/rcjsuen/dockerfile-language-server-nodejs
+  "ruff_lsp",      -- https://github.com/astral-sh/ruff-lsp
+  "rust_analyzer", -- https://github.com/rust-lang/rust-analyzer
+  "tsserver",      -- https://github.com/typescript-language-server/typescript-language-server
+}
+
+for _, lsp in pairs(servers) do
+  lspconfig[lsp].setup({
+    on_attach = M.on_attach,
+    capabilites = M.capabilities,
+  })
+end
+
+
 -- Jedi Language Server
 -- https://github.com/pappasam/jedi-language-server
 lspconfig.jedi_language_server.setup({
@@ -16,27 +29,6 @@ lspconfig.jedi_language_server.setup({
   },
 })
 
--- Ruff Language Server
--- https://github.com/astral-sh/ruff-lsp
-lspconfig.ruff_lsp.setup({
-  on_attach = M.on_attach,
-  capabilities = M.capabilities,
-})
-
---------------------------------
--- Rust LSP Config
---------------------------------
--- Rust Analyzer
--- https://github.com/rust-lang/rust-analyzer
-lspconfig.rust_analyzer.setup({
-  on_attach = M.on_attach,
-  capabilities = M.capabilities,
-})
-
---------------------------------
--- Lua LSP Config
---------------------------------
---  Lua Language Server
 -- https://github.com/LuaLS/lua-language-server
 lspconfig.lua_ls.setup({
   on_attach = M.on_attach,
@@ -55,35 +47,4 @@ lspconfig.lua_ls.setup({
       },
     },
   },
-})
-
-
---------------------------------
--- Bash LSP Config
---------------------------------
---  Lua Language Server
--- https://github.com/bash-lsp/bash-language-server
-lspconfig.bashls.setup({
-  on_attach = M.on_attach,
-  capabilities = M.capabilities,
-})
-
---------------------------------
--- Javascript LSP Config
---------------------------------
--- Typescript Language Server
--- https://github.com/typescript-language-server/typescript-language-server
-lspconfig.tsserver.setup({
-  on_attach = M.on_attach,
-  capabilities = M.capabilities,
-})
-
---------------------------------
--- Docker LSP Config
---------------------------------
--- Dockerfile Language Server
--- https://github.com/rcjsuen/dockerfile-language-server-nodejs
-lspconfig.dockerls.setup({
-  on_attach = M.on_attach,
-  capabilities = M.capabilities,
 })
