41f59e0ff036e1a65dee741d6232fc5c3992611f
Gabriel Morales <gabrielmorales1@protonmail.com>
2024-05-31 00:59:52 -0500

    [nvim] - Update plugins

--- .config/nvim/lua/plugins/buffer-manager.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "j-morano/buffer_manager.nvim",
-  config = function()
-    local bmui = require("buffer_manager.ui")
-    local opts = { noremap = true }
-
-    vim.keymap.set("n", "<leader>b", bmui.toggle_quick_menu, opts)
-  end
-}
