d7202954316a8e23186f1fb6a03cf22866ea4f1f
Gabriel Morales <gabrielmorales1@protonmail.com>
2024-07-30 00:36:34 -0500

    [nvim] - folding support

---
+++ .config/nvim/lua/plugins/nvim-ufo.lua
@@ -0,0 +1,16 @@
+return {
+  "kevinhwang91/nvim-ufo",
+  dependencies = {
+    "kevinhwang91/promise-async",
+  },
+  opts = {
+    provider_selector = function(bufnr, filetype, buftype)
+      return { 'treesitter', 'indent' }
+    end
+  },
+  config = function(_, opts)
+    vim.o.foldlevel = 99
+    vim.o.foldlevelstart = 99
+    require("ufo").setup(opts)
+  end
+}
