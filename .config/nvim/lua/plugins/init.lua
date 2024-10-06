e2bac93efc61450a8d7b9c9d3d415020947f5b14
Gabriel Morales <gabrielmorales1@protonmail.com>
2024-10-06 15:35:24 -0500

    [nivm] - adds GitHub Copilot plugin

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,8 +1,6 @@
 return {
   "gpanders/editorconfig.nvim",
   "norcalli/nvim-colorizer.lua",
-  {
-    "Vimjas/vim-python-pep8-indent",
-    ft = { "python" },
-  },
+  "Vimjas/vim-python-pep8-indent",
+  "github/copilot.vim",
 }
