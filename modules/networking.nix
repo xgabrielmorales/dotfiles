5907f61545a30861b02c17f923b185433b8b60a3
Gabriel Morales <gabrielmorales1@protonmail.com>
2025-11-03 03:28:54 -0500

    [os] - disable netowrkmanager and dchp

--- modules/networking.nix
+++ modules/networking.nix
@@ -2,9 +2,15 @@
 
 {
   networking = {
+    defaultGateway = "192.168.1.1";
     hostName = "xgm";
+    interfaces.enp8s0.ipv4.addresses = [{
+      address = "192.168.1.3";
+      prefixLength = 24;
+    }];
     nameservers = [ "192.168.1.2" ];
-    networkmanager.enable = true;
+    networkmanager.enable = false;
+    useDHCP = false;
   };
   programs.ssh.startAgent = true;
 }
