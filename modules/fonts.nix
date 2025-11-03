ee6e03d1b1a99b0572b53078c239cd0dfddecee9
Gabriel Morales <gabrielmorales1@protonmail.com>
2025-11-02 23:08:11 -0500

    [os] - font config

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -1,10 +1,21 @@
 { pkgs, ... }:
 
 {
-  fonts.packages = with pkgs; [
-    cantarell-fonts
-    icomoon-feather
-    nerd-fonts.jetbrains-mono
-    twitter-color-emoji
-  ];
+  fonts = {
+    packages = with pkgs; [
+      cantarell-fonts
+      icomoon-feather
+      nerd-fonts.jetbrains-mono
+      noto-fonts
+      twitter-color-emoji
+    ];
+    fontconfig = {
+      enable = true;
+      defaultFonts = {
+        monospace = [ "JetBrainsMono Nerd Font" ];
+        sansSerif = [ "Noto Sans" ];
+        serif = [ "Noto Serif" ];
+      };
+    };
+  };
 }
