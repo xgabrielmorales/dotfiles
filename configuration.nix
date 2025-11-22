fedb1c4d8a7909a1eec0b30a291ff3bab5e161e0
Gabriel Morales <gabrielmorales1@protonmail.com>
2025-11-21 20:44:22 -0500

    [os] - reorganize modules into directory structure with default.nix

--- configuration.nix
+++ configuration.nix
@@ -3,17 +3,17 @@
 {
   imports = [
     /etc/nixos/hardware-configuration.nix
-    ./modules/main-user.nix
-    ./modules/boot.nix
-    ./modules/networking.nix
-    ./modules/bluetooth.nix
-    ./modules/locale.nix
-    ./modules/desktop.nix
-    ./modules/packages.nix
-    ./modules/fonts.nix
-    ./modules/users.nix
-    ./modules/development.nix
-    ./modules/nvidia.nix
+    ./modules/main-user
+    ./modules/boot
+    ./modules/networking
+    ./modules/bluetooth
+    ./modules/locale
+    ./modules/desktop
+    ./modules/packages
+    ./modules/fonts
+    ./modules/users
+    ./modules/development
+    ./modules/nvidia
   ];
 
   system.stateVersion = "25.05";
