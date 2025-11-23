93c75b92c3a6489a027f83203ca68a31045d0922
Gabriel Morales <gabrielmorales1@protonmail.com>
2025-11-22 22:59:55 -0500

    [os] - update network configuration to 10.10.10.0/24 subnet

--- modules/networking/default.nix
+++ modules/networking/default.nix
@@ -2,13 +2,13 @@
 
 {
   networking = {
-    defaultGateway = "192.168.1.1";
+    defaultGateway = "10.10.10.1";
     hostName = "xgm";
     interfaces.enp8s0.ipv4.addresses = [{
-      address = "192.168.1.3";
+      address = "10.10.10.4";
       prefixLength = 24;
     }];
-    nameservers = [ "192.168.1.2" ];
+    nameservers = [ "10.10.10.2" ];
     networkmanager.enable = false;
     useDHCP = false;
   };
