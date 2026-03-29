b458d1e02b8cc16837fa9c1ad18ad746fd9a5dac
Gabriel Morales <gabrielmorales1@protonmail.com>
2026-03-29 02:15:34 -0500

    [nvim] - add keymaps to copy file paths to clipboard

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -11,3 +11,7 @@ map("n", "<ESC><ESC>", ":nohlsearch<CR>", options)
 -- Page down/up and center view
 map("n", "<C-d>", "<C-d>zz", options)
 map("n", "<C-u>", "<C-u>zz", options)
+
+-- Copy relative or absolute path to clipboard
+map("n", "<leader>crp", ":let @+ = expand('%:t')<CR>", options)
+map("n", "<leader>cap", ":let @+ = expand('%:p')<CR>", options)
