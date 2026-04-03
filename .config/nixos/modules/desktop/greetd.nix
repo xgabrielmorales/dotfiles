7dbb23c80a62ce4266ce60a9618feb55dc61780f
Gabriel Morales <gabrielmorales1@protonmail.com>
2026-04-03 14:01:56 -0500

    [os] - enable session remembering in greetd tuigreet

--- .config/nixos/modules/desktop/greetd.nix
+++ .config/nixos/modules/desktop/greetd.nix
@@ -6,7 +6,7 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd labwc";
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --cmd labwc";
         user = "greeter";
       };
     };
