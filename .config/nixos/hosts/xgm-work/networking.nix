8e75bfb567b2eb8ac2244aa0f11a624d17e70637
Gabriel Morales <gabrielmorales1@protonmail.com>
2026-04-12 17:43:40 -0500

    [os] - replace dhcpcd with systemd-networkd for iphone usb tethering

--- .config/nixos/hosts/xgm-work/networking.nix
+++ .config/nixos/hosts/xgm-work/networking.nix
@@ -4,6 +4,7 @@
   networking = {
     hostName = "xgm-work";
     networkmanager.enable = false;
+    dhcpcd.enable = false;
     useDHCP = false;
     nameservers = [
       "1.1.1.1"
--- .config/nixos/modules/hardware/iphone-tethering.nix
+++ .config/nixos/modules/hardware/iphone-tethering.nix
@@ -4,8 +4,13 @@
   # usbmuxd is required for iOS USB communication (tethering, file access, etc.)
   services.usbmuxd.enable = true;
 
-  # Enable DHCP on iPhone USB network interfaces
-  networking.interfaces."eth0".useDHCP = true;
+  # Enable systemd-networkd for iPhone USB network interface
+  systemd.network.enable = true;
+  systemd.network.networks."50-iphone-usb" = {
+    matchConfig.Name = "eth0";
+    networkConfig.DHCP = "yes";
+    linkConfig.RequiredForOnline = "no";
+  };
 
   environment.systemPackages = with pkgs; [
     libimobiledevice # CLI tools for interacting with iOS devices
