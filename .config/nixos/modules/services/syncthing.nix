93f762d9fdcaddbb2a80e10912adb7c2f1702323
Gabriel Morales <gabrielmorales1@protonmail.com>
2026-04-13 17:19:22 -0500

    [os] - add firewall rules for syncthing

--- .config/nixos/modules/services/syncthing.nix
+++ .config/nixos/modules/services/syncthing.nix
@@ -1,10 +1,14 @@
 { mainUser, ... }:
 
 {
-  # Servicios
   services.syncthing = {
     enable = true;
     user = mainUser;
     dataDir = "/home/${mainUser}";
+  };
+
+  networking.firewall = {
+    allowedTCPPorts = [ 22000 ];
+    allowedUDPPorts = [ 22000 21027 ];
   };
 }
