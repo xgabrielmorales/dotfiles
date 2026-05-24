882a2fd8c09fb2836cad0ed5ce6cc0f780e8a08c
Gabriel Morales <gabrielmorales1@protonmail.com>
2026-05-24 10:45:53 -0500

    [os] - implement autologin

--- .config/nixos/modules/desktop/greetd.nix
+++ .config/nixos/modules/desktop/greetd.nix
@@ -1,12 +1,12 @@
-{ pkgs, ... }:
+{ mainUser, ... }:
 
 {
   services.greetd = {
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --cmd labwc";
-        user = "greeter";
+        command = "labwc";
+        user = mainUser;
       };
     };
   };
