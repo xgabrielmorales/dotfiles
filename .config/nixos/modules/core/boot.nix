6d930691f7666014172945c866823b47aceca022
Gabriel Morales <gabrielmorales1@protonmail.com>
2026-06-17 00:33:00 -0500

    [os] - add Plymouth boot splash

--- .config/nixos/modules/core/boot.nix
+++ .config/nixos/modules/core/boot.nix
@@ -5,8 +5,10 @@
   boot.initrd.verbose = false;
   boot.kernelParams = [
     "quiet"
+    "splash"
     "udev.log_level=3"
   ];
+  boot.plymouth.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.systemd-boot.configurationLimit = 5;
   boot.loader.systemd-boot.enable = true;
