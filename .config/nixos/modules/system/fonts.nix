320ed60c40eef31f17a5bc71317d487cd09dec38
Gabriel Morales <gabrielmorales1@protonmail.com>
2026-06-20 14:51:31 -0500

    [fonts] - add CJK fonts for asian character support

--- .config/nixos/modules/system/fonts.nix
+++ .config/nixos/modules/system/fonts.nix
@@ -7,6 +7,8 @@
       cantarell-fonts
       nerd-fonts.jetbrains-mono
       noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-cjk-serif
       twitter-color-emoji
     ];
     fontconfig = {
