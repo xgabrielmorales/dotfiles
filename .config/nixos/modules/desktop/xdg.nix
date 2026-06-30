8fffcf0e562f6f0cf597790f93a48d8eb2adf6c4
Gabriel Morales <gabrielmorales1@protonmail.com>
2026-06-30 12:17:32 -0500

    [xdg] - configure wlr screencast portal for screen sharing

--- .config/nixos/modules/desktop/xdg.nix
+++ .config/nixos/modules/desktop/xdg.nix
@@ -5,5 +5,14 @@
     enable = true;
     extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
     config.common.default = "*";
+    wlr = {
+      enable = true;
+      settings.screencast = {
+        chooser_type = "none";
+        max_fps = 30;
+        exec_before = "${pkgs.libnotify}/bin/notify-send -u normal -i video-display 'Screen sharing' 'You started sharing your screen'";
+        exec_after = "${pkgs.libnotify}/bin/notify-send -u normal -i video-display 'Screen sharing' 'You stopped sharing your screen'";
+      };
+    };
   };
 }
