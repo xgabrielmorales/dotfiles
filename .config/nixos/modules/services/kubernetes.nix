c710daf1916ce259fa65ba5196cdae4cfb458821
Gabriel Morales <gabrielmorales1@protonmail.com>
2026-07-11 01:42:57 -0500

    [os] - remove k3s service from kubernetes module and add cli tools
    Disable the k3s server role, firewall port, and kubeconfig env var; keep
    only client-side packages (fluxcd, kompose, kubectl, kubernetes-helm,
    kustomize, kustomize-sops).

--- .config/nixos/modules/services/kubernetes.nix
+++ .config/nixos/modules/services/kubernetes.nix
@@ -1,26 +1,12 @@
 { pkgs, ... }:
 
 {
-  services.k3s = {
-    enable = true;
-    role = "server";
-    extraFlags = toString [
-      "--write-kubeconfig-mode=644"
-    ];
-  };
-  networking.firewall = {
-    allowedTCPPorts = [
-      6443 # Kubernetes API server
-    ];
-  };
-
   environment.systemPackages = with pkgs; [
+    fluxcd
+    kompose
+    kubectl
     kubernetes-helm
     kustomize
     kustomize-sops
   ];
-
-  environment.sessionVariables = {
-    KUBECONFIG = "/etc/rancher/k3s/k3s.yaml";
-  };
 }
