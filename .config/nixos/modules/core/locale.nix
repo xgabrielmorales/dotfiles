96a7f387806faf3619548b468644f94f30071520
Gabriel Morales <gabrielmorales1@protonmail.com>
2026-08-12 23:43:23 -0500

    [os] - add font for console

--- .config/nixos/modules/core/locale.nix
+++ .config/nixos/modules/core/locale.nix
@@ -1,8 +1,14 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
-  time.timeZone = "America/Bogota";
-  time.hardwareClockInLocalTime = true;
-  console.enable = true;
-  console.useXkbConfig = true;
+  time = {
+    timeZone = "America/Bogota";
+    hardwareClockInLocalTime = true;
+  };
+  console = {
+    enable = true;
+    useXkbConfig = true;
+    font = "ter-v24n";
+    packages = [ pkgs.terminus_font ];
+  };
 }
