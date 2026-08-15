a7ab60bb69c7ebdb74a40308969f66cf9c4c4b8e
Gabriel Morales <gabrielmorales1@protonmail.com>
2026-08-14 21:17:04 -0500

    [os] - remap ctrl to ralt and rctrl

--- .config/nixos/hosts/xgm-work/keyboard.nix
+++ .config/nixos/hosts/xgm-work/keyboard.nix
@@ -2,7 +2,9 @@
 
 {
   services.xserver.xkb.layout = "latam";
+  services.xserver.xkb.options = "ctrl:ralt_rctrl";
   environment.sessionVariables = {
     XKB_DEFAULT_LAYOUT = "latam";
+    XKB_DEFAULT_OPTIONS = "ctrl:ralt_rctrl";
   };
 }
