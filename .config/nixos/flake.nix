f1db8d23b8df02e12b4a7ea38944cf9250f187ea
Gabriel Morales <gabrielmorales1@protonmail.com>
2026-08-16 09:58:56 -0500

    [os] - remove unused flake inputs

--- .config/nixos/flake.nix
+++ .config/nixos/flake.nix
@@ -11,16 +11,8 @@
       url = "github:0xc000022070/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nix-index-database = {
-      url = "github:nix-community/nix-index-database";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     sops-nix = {
       url = "github:Mic92/sops-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    claude-desktop = {
-      url = "github:aaddrick/claude-desktop-debian";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -30,13 +22,10 @@
       nixpkgs-2605,
       zen-browser,
       home-manager,
-      nix-index-database,
       sops-nix,
-      claude-desktop,
       ...
     }:
     let
-      # Hostname and main user share the same name on every machine
       mkHost =
         name:
         nixpkgs.lib.nixosSystem {
@@ -48,12 +37,10 @@
           modules = [
             ./hosts/${name}
             home-manager.nixosModules.home-manager
-            nix-index-database.nixosModules.default
             sops-nix.nixosModules.sops
             {
               nixpkgs.overlays = [
                 (import ./overlays)
-                claude-desktop.overlays.default
                 (final: _: {
                   inherit (nixpkgs-2605.legacyPackages.${final.system}) arc-theme;
                 })
