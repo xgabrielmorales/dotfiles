f6c7fd809ad0d945c1e7a62994554c8cc00603ee
Gabriel Morales <gabrielmorales1@protonmail.com>
2026-08-18 00:21:38 -0500

    [os] - map numpad Enter to AltGr on xgm-work

--- .config/nixos/hosts/xgm-work/keyboard.nix
+++ .config/nixos/hosts/xgm-work/keyboard.nix
@@ -2,9 +2,9 @@
 
 {
   services.xserver.xkb.layout = "latam";
-  services.xserver.xkb.options = "ctrl:ralt_rctrl";
+  services.xserver.xkb.options = "ctrl:ralt_rctrl,lv3:enter_switch";
   environment.sessionVariables = {
     XKB_DEFAULT_LAYOUT = "latam";
-    XKB_DEFAULT_OPTIONS = "ctrl:ralt_rctrl";
+    XKB_DEFAULT_OPTIONS = "ctrl:ralt_rctrl,lv3:enter_switch";
   };
 }
