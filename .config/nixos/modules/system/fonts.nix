d0c444f15413f7092be6ac3f7355770416b8b4ad
Gabriel Morales <gabrielmorales1@protonmail.com>
2026-09-16 09:12:29 -0500

    [os] - force color emoji font over monochrome fallbacks

--- .config/nixos/modules/system/fonts.nix
+++ .config/nixos/modules/system/fonts.nix
@@ -26,6 +26,17 @@
         ];
         emoji = [ "Twitter Color Emoji" ];
       };
+      localConf = ''
+        <?xml version="1.0"?>
+        <!DOCTYPE fontconfig SYSTEM "urn:fontconfig:fonts.dtd">
+        <fontconfig>
+          <match target="pattern">
+            <edit name="family" mode="append" binding="strong">
+              <string>Twitter Color Emoji</string>
+            </edit>
+          </match>
+        </fontconfig>
+      '';
     };
   };
 }
