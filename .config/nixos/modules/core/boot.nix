1f06abed7aef24579a91937f7d58cefe149f2dbd
Gabriel Morales <gabrielmorales1@protonmail.com>
2026-09-19 00:52:54 -0500

    [os] - disable systemd-boot kernel cmdline editor

--- .config/nixos/modules/core/boot.nix
+++ .config/nixos/modules/core/boot.nix
@@ -12,6 +12,7 @@
   boot.plymouth.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.systemd-boot.configurationLimit = 5;
+  boot.loader.systemd-boot.editor = false;
   boot.loader.systemd-boot.enable = true;
   boot.loader.timeout = 5;
   boot.tmp.cleanOnBoot = true;
